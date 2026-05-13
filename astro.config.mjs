c5404a76d1dbd7e1b0543029f6823926bb1d626b
tanya <tannyfairy@gmail.com>
2026-05-12 20:37:14 -0400

    Updated astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gmztanya.github.io',
-  base: '/fkhatib',
   vite: {
     plugins: [tailwindcss()]
   }
