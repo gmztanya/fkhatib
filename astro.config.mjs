8de62c805e757e6a5061b312dda0516e5c263466
tanya <tannyfairy@gmail.com>
2026-05-12 20:27:53 -0400

    Setup Astro GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gmztanya.github.io',
+  base: '/fkhatib',
   vite: {
     plugins: [tailwindcss()]
   }
